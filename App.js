00105f7e2fef09c3e7dd1dd383dfc2c832cd5a87
“csureshkumargit” <csureshkumargce@gmail.com>
2022-02-08 04:02:04 -0500

    Initial Checkin

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const host = "localhost";
+//const host = "localhost";
 const PORT = process.env.PORT || 9092;
 const products = require('./Routers/products');
 const statistics = require('./Routers/statistics');
@@ -14,7 +14,7 @@ app.use('/products', products);
 app.use('/statistics', statistics);
 mongoose.connect(process.env.SERVER_MONGO_URL).
     then(() => {
-        app.listen(PORT, host, () => {
-            console.log(`The service is running @ ${host}:${PORT}`);
+        app.listen(PORT, () => {
+            console.log(`The service is running @ ${PORT}`);
         });
     }).catch(err => console.log('DB not connected'));
